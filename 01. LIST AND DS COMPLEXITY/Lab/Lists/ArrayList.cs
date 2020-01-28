8a27874f303f69e0ad5c05b23a0ef23b2c8dfe76
Zdravko Zdravkov <pirocorp@abv.bg>
2020-01-28 19:05:09 +0100

    Lab

--- 01. LIST AND DS COMPLEXITY/Lab/Lists/ArrayList.cs
+++ 01. LIST AND DS COMPLEXITY/Lab/Lists/ArrayList.cs
@@ -2,28 +2,79 @@
 
 public class ArrayList<T>
 {
-    public int Count { get; set; }
+    private const int INITIAL_CAPACITY = 2;
+
+    private T[] items;
+
+    public int Count { get; private set; }
+
+    public ArrayList()
+    {
+        this.items = new T[INITIAL_CAPACITY];
+        this.Count = 0;
+    }
 
     public T this[int index]
     {
         get
         {
-            throw new NotImplementedException();
+            this.IndexCheck(index);
+            return this.items[index];
         }
 
         set
         {
-            throw new NotImplementedException();
+            this.IndexCheck(index);
+            this.items[index] = value;
         }
     }
 
     public void Add(T item)
     {
-        throw new NotImplementedException();
+        if (this.Count >= this.items.Length)
+        {
+            this.Resize(this.Count * 2);
+        }
+
+        this.items[this.Count] = item;
+        this.Count++;
     }
 
     public T RemoveAt(int index)
     {
-        throw new NotImplementedException();
+        this.IndexCheck(index);
+
+        T item = this.items[index];
+
+        for (var i = index; i < this.Count - 1; i++)
+        {
+            this.items[i] = this.items[i + 1];
+        }
+
+        this.items[this.Count - 1] = default(T);
+        this.Count--;
+
+        if (this.Count <= this.items.Length / 4 &&
+            this.items.Length / 4 > 0)
+        {
+            this.Resize(this.items.Length / 2);
+        }
+
+        return item;
+    }
+
+    private void IndexCheck(int index)
+    {
+        if (index < 0 || index >= this.Count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    private void Resize(int newSize)
+    {
+        T[] newArray = new T[newSize];
+        Array.Copy(this.items, newArray, this.Count);
+        this.items = newArray;
     }
 }
