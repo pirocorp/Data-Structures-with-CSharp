71200cc423732e0cd5ae0580db2cd6aa472433cb
Zdravko Zdravkov <zdr0@yahoo.com>
2020-01-28 17:21:58 +0100

    01. Algorithms And Complexity

---
+++ 01. LIST AND DS COMPLEXITY/01. Algorithms And Complexity/01. Compare Execution Speed/01. Compare Execution Speed/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _01._Compare_Execution_Speed
+{
+    using System;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            var startTime = DateTime.Now;
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                IsPrimeFast(i);
+            }
+
+            var executionTime = DateTime.Now - startTime;
+            Console.WriteLine(executionTime);
+        }
+
+        static bool IsPrime(long num)
+        {
+            for (var i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static bool IsPrimeFast(long num)
+        {
+            int maxDivisor = (int)Math.Sqrt(num);
+            for (int i = 2; i <= maxDivisor; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
