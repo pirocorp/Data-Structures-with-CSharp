659baa59a4e099c332090f28135742503d7502db
Zdravko Zdravkov <zdr0@yahoo.com>
2020-01-30 15:42:02 +0100

    01. Reverse Numbers

---
+++ 02. STACKS AND QUEUES/EXERCISE/01. Reverse Numbers/ReverseNumbersProgram.cs
@@ -0,0 +1,27 @@
+﻿namespace _01._Reverse_Numbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class ReverseNumbersProgram
+    {
+        public static void Main()
+        {
+            var inputNumbers = Console.ReadLine()
+                .Split(' ')
+                .Where(x => x != "")
+                .Select(int.Parse)
+                .ToArray();
+
+            var stack = new Stack<int>();
+
+            foreach (var num in inputNumbers)
+            {
+                stack.Push(num);
+            }
+
+            Console.WriteLine(string.Join(" ", stack));
+        }
+    }
+}
