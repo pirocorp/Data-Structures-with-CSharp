e11d1c87933b4e975aef08a7f55a5ba1e6322759
Zdravko Zdravkov <zdr0@yahoo.com>
2020-01-30 16:07:19 +0100

    02. Calculate Sequence

---
+++ 02. STACKS AND QUEUES/EXERCISE/02. Calculate Sequence/CalculateSequenceProgram.cs
@@ -0,0 +1,43 @@
+﻿namespace _02._Calculate_Sequence
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class CalculateSequenceProgram
+    {
+        public static void Main()
+        {
+            var queue = new Queue<int>();
+
+            var input = int.Parse(Console.ReadLine());
+            var items = new List<int>();
+
+            queue.Enqueue(input);
+            items.Add(input);
+
+            AddElementsToQueue(queue, items);
+            items = items.Take(50).ToList();
+
+            Console.WriteLine(string.Join(", ", items));
+        }
+
+        private static void AddElementsToQueue(Queue<int> queue, List<int> items)
+        {
+            var current = queue.Dequeue();
+
+            queue.Enqueue(current + 1);
+            items.Add(current + 1);
+            queue.Enqueue(2 * current + 1);
+            items.Add(2 * current + 1);
+            queue.Enqueue(current + 2);
+            items.Add(current + 2);
+
+
+            if (items.Count < 50)
+            {
+                AddElementsToQueue(queue, items);
+            }
+        }
+    }
+}
