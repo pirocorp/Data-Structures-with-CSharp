21667a0237a4e5f186adc0592733dc5b9434816a
Zdravko Zdravkov <pirocorp@abv.bg>
2020-01-30 16:49:25 +0100

    03. Array Stack

---
+++ 02. STACKS AND QUEUES/EXERCISE/03. Array Stack/ArrayStack.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class ArrayStack<T>
+{
+    private const int INITIAL_CAPACITY = 4;
+
+    private T[] elements;
+
+    public int Count { get; private set; }
+
+    public int Capacity => this.elements.Length;
+
+    public ArrayStack(int capacity = INITIAL_CAPACITY)
+    {
+        this.elements = new T[capacity];
+        this.Count = 0;
+    }
+
+    public void Push(T element)
+    {
+        if (this.Count == this.elements.Length)
+        {
+            this.Grow();
+        }
+
+        this.elements[this.Count] = element;
+        this.Count++;
+    }
+
+    public T Pop()
+    {
+        if (this.Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        var element = this.elements[this.Count - 1];
+        this.elements[this.Count - 1] = default(T);
+        this.Count--;
+
+        return element;
+    }
+
+    public T[] ToArray()
+    {
+        var array = new T[this.Count];
+        var destinationIndex = 0;
+
+        for (var sourceIndex = this.Count - 1; sourceIndex >= 0; sourceIndex--)
+        {
+            array[destinationIndex++] = this.elements[sourceIndex];
+        }
+
+        return array;
+    }
+
+    private void Grow()
+    {
+        var newElements = new T[this.elements.Length * 2];
+        Array.Copy(this.elements, newElements, this.elements.Length);
+        this.elements = newElements;
+    }
+}
--- 02. STACKS AND QUEUES/EXERCISE/03. Array Stack/ArrayStackProgram.cs
+++ 02. STACKS AND QUEUES/EXERCISE/03. Array Stack/ArrayStackProgram.cs
@@ -6,6 +6,21 @@
     {
         public static void Main()
         {
+            var stack = new ArrayStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            Console.WriteLine(string.Join(", ", stack.ToArray()));
+
+            var x = stack.Pop();
+            Console.WriteLine(x);
+            Console.WriteLine(string.Join(", ", stack.ToArray()));
+            var y = stack.Pop();
+            Console.WriteLine(y);
+            Console.WriteLine(string.Join(", ", stack.ToArray()));
         }
     }
 }
