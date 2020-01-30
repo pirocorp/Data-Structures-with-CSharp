49213e29d8cd1626b04d9855fe59a7b7cde1832a
Zdravko Zdravkov <pirocorp@abv.bg>
2020-01-30 17:51:27 +0100

    05. Linked Queue

---
+++ 02. STACKS AND QUEUES/EXERCISE/05. Linked Queue/LinkedQueue.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq.Expressions;
+
+public class LinkedQueue<T>
+{
+    private QueueNode head;
+    private QueueNode tail;
+
+    public int Count { get; private set; }
+
+    public LinkedQueue()
+    {
+        this.head = null;
+        this.tail = null;
+        this.Count = 0;
+    }
+
+    public void Enqueue(T element)
+    {
+        var newTail = new QueueNode(element, this.tail, null);
+
+        if (this.Count == 0)
+        {
+            this.tail = newTail;
+            this.head = this.tail;
+        }
+        else
+        {
+            this.tail.NextNode = newTail;
+            this.tail = newTail;
+        }
+
+        this.Count++;
+    }
+
+    public T Dequeue()
+    {
+        if (this.Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        var element = this.head.Value;
+
+        this.head = this.head.NextNode;
+        this.Count--;
+
+        return element;
+    }
+
+    public T[] ToArray()
+    {
+        var array = new T[this.Count];
+
+        var current = this.head;
+        var index = 0;
+
+        while (current != null)
+        {
+            array[index++] = current.Value;
+            current = current.NextNode;
+        }
+
+        return array;
+    }
+
+    private class QueueNode
+    {
+        public T Value { get; private set; }
+
+        public QueueNode NextNode { get; set; }
+
+        public QueueNode PrevNode { get; set; }
+
+        public QueueNode(T value, QueueNode prev, QueueNode next)
+        {
+            this.Value = value;
+            this.PrevNode = prev;
+            this.NextNode = next;
+        }
+    }
+}
---
+++ 02. STACKS AND QUEUES/EXERCISE/05. Linked Queue/LinkedQueueProgram.cs
@@ -0,0 +1,29 @@
+﻿namespace _05.Linked_Queue
+{
+    using System;
+
+    public static class LinkedQueueProgram
+    {
+        public static void Main()
+        {
+            var queue = new LinkedQueue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+
+            Console.WriteLine(string.Join(", ", queue.ToArray()));
+
+            var r = queue.Dequeue();
+            Console.WriteLine(r);
+            Console.WriteLine(string.Join(", ", queue.ToArray()));
+
+
+            r = queue.Dequeue();
+            Console.WriteLine(r);
+            Console.WriteLine(string.Join(", ", queue.ToArray()));
+
+        }
+    }
+}
