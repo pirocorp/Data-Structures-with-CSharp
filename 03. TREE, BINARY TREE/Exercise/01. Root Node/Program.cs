05173459bb697c4ca993bbbf17689d19ae9f8dcd
Zdravko Zdravkov <pirocorp@abv.bg>
2020-02-05 20:35:40 +0100

    01. Root Node - unfinished

---
+++ 03. TREE, BINARY TREE/Exercise/01. Root Node/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+public static class Program
+{
+    static Dictionary<int, Tree<int>> nodes = new Dictionary<int, Tree<int>>();
+
+    public static void Main()
+    {
+        ReadTree();
+    }
+
+    private static void ReadTree()
+    {
+        throw new System.NotImplementedException();
+    }
+}
