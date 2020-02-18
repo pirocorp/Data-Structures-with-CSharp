a84ecc5f0a5bf25998a2bd9a7ed9211615a720ba
Zdravko Zdravkov <zdr0@yahoo.com>
2020-02-18 17:49:18 +0100

    01. Rope

---
+++ 08. ROPE AND TRIE/Demos/01. Rope/Rope.cs
@@ -0,0 +1,131 @@
+﻿namespace _01._Rope
+{
+    using System;
+
+    public class Rope
+    {
+        private RopeNode _root;
+
+        public Rope()
+        {
+            this._root = new RopeNode("");
+        }
+
+        public void Clear()
+        {
+            this._root = new RopeNode("");
+        }
+
+        public void Concat(string str)
+        {
+            var newRope = new RopeNode(str);
+            var newRoot = new RopeNode("");
+
+            newRoot.Left = this._root;
+            newRoot.Right = newRope;
+
+            newRoot.Weight = newRoot.Left.Weight;
+
+            if (newRoot.Left.Right != null)
+            {
+                newRoot.Weight += newRoot.Left.Right.Weight;
+            }
+
+            this._root = newRoot;
+        }
+
+        public char CharacterAt(int index)
+        {
+            var current = this._root;
+
+            if (index > current.Weight)
+            {
+                index -= current.Weight;
+                return current.Right.Data[index];
+            }
+
+            while (index < current.Weight)
+            {
+                current = current.Left;
+            }
+
+            index -= current.Weight;
+            return current.Right.Data[index];
+        }
+
+        public string Substring(int start, int end)
+        {
+            var result = "";
+            var found = false;
+            var current = this._root;
+
+            if (end > current.Weight)
+            {
+                found = true;
+                end -= current.Weight;
+
+                if (start > current.Weight)
+                {
+                    start -= current.Weight;
+                    result = current.Right.Data.Substring(start, end) + result;
+                    return result;
+                }
+
+                result = current.Right.Data.Substring(0, end);
+            }
+
+            current = current.Left;
+
+            while (start < current.Weight)
+            {
+                result = current.Right.Data + result;
+                current = current.Left;
+            }
+
+            start -= current.Weight;
+            result = current.Right.Data.Substring(start) + result;
+
+            return result;
+        }
+
+        public void Print()
+        {
+            this.Print(this._root);
+            Console.WriteLine();
+        }
+
+        private void Print(RopeNode node)
+        {
+            if (node != null)
+            {
+                this.Print(node.Left);
+
+                if (node.Data != null)
+                {
+                    Console.Write(node.Data);
+                }
+
+                this.Print(node.Right);
+            }
+        }
+
+        private class RopeNode
+        {
+            public RopeNode Left { get; set; }
+
+            public RopeNode Right { get; set; }
+
+            public string Data { get; private set; }
+
+            public int Weight { get; set; }
+
+            public RopeNode(string data)
+            {
+                this.Data = data;
+                this.Weight = data.Length;
+                this.Left = null;
+                this.Right = null;
+            }
+        }
+    }
+}
---
+++ 08. ROPE AND TRIE/Demos/01. Rope/RopeProgram.cs
@@ -0,0 +1,54 @@
+﻿namespace _01._Rope
+{
+    using System;
+
+    public static class RopeProgram
+    {
+        public static void Main()
+        {
+            var rope = new Rope();
+            Console.WriteLine("Rope Test");
+            char ch;
+
+            do
+            {
+                Console.WriteLine("\nRope Operations\n");
+                Console.WriteLine("1. concat ");
+                Console.WriteLine("2. get character at index");
+                Console.WriteLine("3. substring");
+                Console.WriteLine("4. clear");
+
+                var choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter string to concat");
+                        rope.Concat(Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter index");
+                        Console.WriteLine("Character at index = " + rope.CharacterAt(int.Parse(Console.ReadLine())));
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter integer start and end limit");
+                        Console.WriteLine("Substring : " + rope.Substring(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));
+                        break;
+                    case 4:
+                        Console.WriteLine("\nRope Cleared\n");
+                        rope.Clear();
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Entry \n ");
+                        break;
+                }
+                /**  Display rope  **/
+                Console.WriteLine("\nRope : ");
+                rope.Print();
+
+                Console.WriteLine("\nDo you want to continue (Type y or n) \n");
+                ch = Console.ReadLine()[0];
+            } while (ch == 'Y' || ch == 'y');
+        }
+    }
+}
