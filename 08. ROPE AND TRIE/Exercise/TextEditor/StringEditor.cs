4a32699749030f411450519c3d825dcefbeff039
Zdravko Zdravkov <pirocorp@abv.bg>
2020-02-19 13:49:35 +0100

    Exercise

---
+++ 08. ROPE AND TRIE/Exercise/TextEditor/StringEditor.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections.Generic;
+using Wintellect.PowerCollections;
+
+public class StringEditor : ITextEditor
+{
+    //Wintellect's BigList is Rope
+    private readonly Trie<BigList<char>> usersString;
+    private readonly Trie<Stack<string>> usersHistory;
+
+    public StringEditor()
+    {
+        this.usersString = new Trie<BigList<char>>();
+        this.usersHistory = new Trie<Stack<string>>();
+    }
+
+    public void Login(string username)
+    {
+        if (this.usersString.Contains(username))
+        {
+            return;
+        }
+
+        this.usersString.Insert(username, new BigList<char>());
+        this.usersHistory.Insert(username, new Stack<string>());
+    }
+
+    public void Logout(string username)
+    {
+        this.usersString.Delete(username);
+        this.usersHistory.Delete(username);
+    }
+
+    public string Print(string username)
+    {
+        if (!this.usersString.Contains(username))
+        {
+            return string.Empty;
+        }
+
+        return string.Join("", this.usersString.GetValue(username));
+    }
+
+    public void Prepend(string username, string str)
+    {
+        if (!this.usersString.Contains(username))
+        {
+            return; 
+        }
+
+        this.SetUndoPoint(username);
+
+        this.usersString.GetValue(username).AddRangeToFront(str.ToCharArray());
+    }
+    
+    public void Clear(string username)
+    {
+        if (!this.usersString.Contains(username))
+        {
+            return;
+        }
+
+        this.SetUndoPoint(username);
+        this.usersString.Insert(username, new BigList<char>());
+    }
+
+    public void Delete(string username, int startIndex, int length)
+    {
+        if (!this.usersString.Contains(username))
+        {
+            return;
+        }
+
+        this.SetUndoPoint(username);
+
+        var currentBigList = this.usersString.GetValue(username);
+        currentBigList.RemoveRange(startIndex, length);
+    }
+
+    public void Insert(string username, int index, string str)
+    {
+        if (!this.usersString.Contains(username))
+        {
+            return;
+        }
+
+        this.SetUndoPoint(username);
+
+        var currentBigList = this.usersString.GetValue(username);
+        currentBigList.InsertRange(index, str);
+    }
+
+    public int Length(string username)
+    {
+        if (!this.usersString.Contains(username))
+        {
+            return 0;
+        }
+
+        return this.usersString.GetValue(username).Count;
+    }
+
+    public void Substring(string username, int startIndex, int length)
+    {
+        if (!this.usersString.Contains(username))
+        {
+            return;
+        }
+
+        this.SetUndoPoint(username);
+
+        var currentBigList = this.usersString.GetValue(username);
+
+        var subBigList = currentBigList.GetRange(startIndex, length);
+        this.usersString.Insert(username, subBigList);
+    }
+
+    public void Undo(string username)
+    {
+        if (!this.usersString.Contains(username))
+        {
+            return;
+        }
+
+        var undoString = this.usersHistory.GetValue(username).Pop();
+
+        var newBigList = new BigList<char>();
+        newBigList.AddRange(undoString);
+
+        this.usersString.Insert(username, newBigList);
+    }
+
+    public IEnumerable<string> Users(string prefix = "")
+    {
+        return this.usersString.GetByPrefix(prefix);
+    }
+
+    private void SetUndoPoint(string username)
+    {
+        this.usersHistory.GetValue(username)
+            .Push(string.Join("", this.usersString.GetValue(username)));
+    }
+}
---
+++ 08. ROPE AND TRIE/Exercise/TextEditor/TextEditorProgram.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+public static class TextEditorProgram
+{
+    public static void Main()
+    {
+        var textEditor = new StringEditor();
+        while (true)
+        {
+            var input = Console.ReadLine();
+            var text = input.Split('"');
+            var commands = input.Split();
+            if (commands[0] == "end")
+            {
+                break;
+            }
+
+            switch (commands[0])
+            {
+                case "login":
+                    textEditor.Login(commands[1]);
+                    break;
+                case "logout":
+                    textEditor.Logout(commands[1]);
+                    break;
+                case "users":
+                    if (commands.Length > 1)
+                    {
+                        foreach (var user in textEditor.Users(commands[1]))
+                        {
+                            Console.WriteLine(user);
+                        }
+                    }
+                    else
+                    {
+                        foreach (var user in textEditor.Users())
+                        {
+                            Console.WriteLine(user);
+                        }
+                    }
+                    break;
+                default:
+                    var name = commands[0];
+                    var command = commands[1];
+
+                    switch (command)
+                    {
+                        case "insert":
+                            var index = int.Parse(commands[2]);
+                            textEditor.Insert(name, index, text[1]);
+                            break;
+                        case "prepend":
+                            textEditor.Prepend(name, text[1]);
+                            break;
+                        case "substring":
+                            var startIndex = int.Parse(commands[2]);
+                            var length = int.Parse(commands[3]);
+                            textEditor.Substring(name, startIndex, length);
+                            break;
+                        case "delete":
+                            var startIndexD = int.Parse(commands[2]);
+                            var lengthD = int.Parse(commands[3]);
+                            textEditor.Delete(name, startIndexD, lengthD);
+                            break;
+                        case "clear":
+                            textEditor.Clear(name);
+                            break;
+                        case "length":
+                            Console.WriteLine(textEditor.Length(name));
+                            break;
+                        case "print":
+                            try
+                            {
+                                Console.WriteLine(textEditor.Print(name));
+                            }
+                            catch (Exception)
+                            {
+                            }
+                            break;
+                        case "undo":
+                            textEditor.Undo(name);
+                            break;
+                        default: throw new InvalidOperationException();
+                    }
+                    break;
+            }
+        }
+    }
+
+    private static void Test()
+    {
+        var stringEditor = new StringEditor();
+        var currentUser = "Pesho";
+        stringEditor.Login(currentUser);
+        stringEditor.Login("Gosho");
+        stringEditor.Login("Pavel");
+        stringEditor.Login("Asen");
+        stringEditor.Prepend(currentUser, "DEF");
+        stringEditor.Prepend(currentUser, " ");
+        stringEditor.Prepend(currentUser, "abc");
+        stringEditor.Prepend(currentUser, " ");
+        stringEditor.Prepend(currentUser, "ZDF");
+        Console.WriteLine(stringEditor.Print(currentUser));
+
+        stringEditor.Delete(currentUser, 1, 2);
+
+        Console.WriteLine(stringEditor.Print(currentUser));
+
+        stringEditor.Insert(currentUser, 1, "_NEW STRING_");
+
+        Console.WriteLine(stringEditor.Print(currentUser));
+
+        Console.WriteLine(stringEditor.Length(currentUser));
+
+        stringEditor.Substring(currentUser, 1, 12);
+
+        Console.WriteLine(stringEditor.Print(currentUser));
+
+        stringEditor.Undo(currentUser);
+
+        Console.WriteLine(stringEditor.Print(currentUser));
+
+        stringEditor.Clear(currentUser);
+
+        Console.WriteLine(stringEditor.Print(currentUser));
+
+        foreach (var user in stringEditor.Users("P"))
+        {
+            Console.WriteLine(user);
+        }
+    }
+}
