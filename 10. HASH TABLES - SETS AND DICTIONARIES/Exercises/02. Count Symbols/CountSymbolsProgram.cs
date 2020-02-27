edd6a51bf61569a6d8e9279011408025a74cf445
Zdravko Zdravkov <pirocorp@abv.bg>
2020-02-27 14:46:34 +0100

    02. Count Symbols

---
+++ 10. HASH TABLES - SETS AND DICTIONARIES/Exercises/02. Count Symbols/CountSymbolsProgram.cs
@@ -0,0 +1,35 @@
+﻿namespace _02.Count_Symbols
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class CountSymbolsProgram
+    {
+        public static void Main()
+        {
+            var dict = new SortedDictionary<char, int>();
+
+            var input = Console.ReadLine();
+
+            if (input == null)
+            {
+                return;
+            }
+
+            foreach (var @char in input)
+            {
+                if (!dict.ContainsKey(@char))
+                {
+                    dict[@char] = 0;
+                }
+
+                dict[@char]++;
+            }
+
+            foreach (var kvp in dict)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+            }
+        }
+    }
+}
