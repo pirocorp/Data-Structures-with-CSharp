ba3d71cbd013109661c8257c300cda09607258b4
Zdravko Zdravkov <pirocorp@abv.bg>
2020-02-27 16:36:00 +0100

    04. Ordered Set RedBlackTree - by Wintellect

---
+++ 10. HASH TABLES - SETS AND DICTIONARIES/Exercises/04. Ordered Set/OrderedSet.cs
@@ -0,0 +1,56 @@
+﻿namespace _04.Ordered_Set
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class OrderedSet<T> : IEnumerable<T> where T : IComparable
+    {
+        private readonly RedBlackTree<T> _tree;
+
+        public OrderedSet()
+        {
+            this._tree = new RedBlackTree<T>(new Comparer());
+        }
+
+        public void Add(T key)
+        {
+            this._tree.Insert(key, DuplicatePolicy.DoNothing, out var x);
+        }
+
+        public bool Contains(T key)
+        {
+            return this._tree.Find(key, true, false, out var element);
+        }
+
+        public void Remove(T key)
+        {
+            this._tree.Delete(key, true, out var deleted);
+        }
+
+        public int Count => this._tree.ElementCount;
+        
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this._tree.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private class Comparer : IComparer<T>
+        {
+            public int Compare(T x, T y)
+            {
+                if (x == null || y == null)
+                {
+                    throw new NullReferenceException();
+                }
+
+                return x.CompareTo(y);
+            }
+        }
+    }
+}
---
+++ 10. HASH TABLES - SETS AND DICTIONARIES/Exercises/04. Ordered Set/OrderedSetProgram.cs
@@ -0,0 +1,33 @@
+﻿namespace _04.Ordered_Set
+{
+    using System;
+
+    public static class OrderedSetProgram
+    {
+        public static void Main()
+        {
+            var set = new OrderedSet<int> {17, 17};
+
+            Console.WriteLine(set.Contains(17));
+            Console.WriteLine(set.Contains(18));
+
+            set.Add(9);
+            Console.WriteLine(set.Contains(9));
+            set.Remove(9);
+            Console.WriteLine(set.Contains(9));
+            
+            set.Add(9);
+            set.Add(12);
+            set.Add(19);
+            set.Add(6);
+            set.Add(25);
+            Console.WriteLine(set.Count);
+            Console.WriteLine();
+
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
